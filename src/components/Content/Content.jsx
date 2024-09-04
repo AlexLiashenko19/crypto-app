eaf75dbf1da7d10398b469d57ee203017ae65613
unknown <sanko.lialko14@gmail.com>
2024-09-04 15:09:42 +0300

    structure

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,18 @@
+import { Layout } from 'antd';
+
+const contentStyle = {
+    textAlign: 'center',
+    minHeight: 'calc(100vh - 60px)',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#001529',
+    padding: '1rem'
+  };
+
+const Content = () => {
+  return (
+    <Layout.Content style={contentStyle}>Content</Layout.Content>
+  )
+}
+
+export default Content
